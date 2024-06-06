962b15609f6573608cde3b3736d3e66868e94e30
superMachopeludo <willian_perin@estudante.sesisenai.org.br>
2024-06-05 21:45:40 -0300

    condicional

---
+++ 02aula-operacao/src/condicional.js
@@ -0,0 +1,27 @@
+// if/else
+
+var x = 10; 
+if(x < 100){
+    console.log("menor que cem..")
+}
+if(x < 1000){
+    console.log("menor que mil")
+}
+
+switch(1){ 
+    case 1:
+        //codigo
+        break;
+    case 2:
+        //codigo
+        break;
+    default:
+        //codigo:
+         break
+}
+
+
+
+
+
+
---
+++ 02aula-operacao/src/index.js
@@ -0,0 +1,12 @@
+  import leia from "readline-sync"
+  
+  var corrida1=leia.questionFloat("valor da primeira corrida:");
+  var corrida2=leia.questionFloat("valor da segunda corrida:");
+  var corrida3=leia.questionFloat("valor da terceira corrida:");
+  var corrida4=leia.questionFloat("valor da quarta corrida:");
+  var corrida5=leia.questionFloat("valor da quinta corrida:");
+  
+  var total = (corrida1 + corrida2 + corrida3 + corrida4 + corrida5)
+  var porcentagem= total - (total *0,25);
+  
+  console.log("valor liquido": + valor liquido)
